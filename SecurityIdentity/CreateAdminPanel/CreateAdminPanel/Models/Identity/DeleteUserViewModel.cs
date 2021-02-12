db0237235b19a51bfc4d6ab2a771a90e0a295566
mindball <mincho.balaliew@gmail.com>
2021-02-12 15:33:53 +0200

    Finish admin panel. CRUD users. Add role to users

---
+++ SecurityIdentity/CreateAdminPanel/CreateAdminPanel/Models/Identity/DeleteUserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CreateAdminPanel.Models.Identity
+{
+    public class DeleteUserViewModel
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
