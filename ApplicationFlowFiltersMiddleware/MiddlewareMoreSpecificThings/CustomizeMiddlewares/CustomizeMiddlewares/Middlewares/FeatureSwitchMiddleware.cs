17be57588b9c03294993576d16b2427df907b018
mindball <mincho.balaliew@gmail.com>
2021-03-02 14:11:48 +0200

    more more specific middlewares

---
+++ ApplicationFlowFiltersMiddleware/MiddlewareMoreSpecificThings/CustomizeMiddlewares/CustomizeMiddlewares/Middlewares/FeatureSwitchMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomizeMiddlewares.Middlewares
+{
+    public class FeatureSwitchMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FeatureSwitchMiddleware(RequestDelegate _next)
+        {
+            this._next = _next;
+        }
+
+        public async Task Invoke(HttpContext context, IConfiguration config)
+        {
+            if (context.Request.Path.Value.Contains("/features"))
+            {
+                var switches = config.GetSection("FeatureSwitches");
+
+                var report = switches.GetChildren().Select(x => $"{x.Key} : {x.Value}");
+
+                await context.Response.WriteAsync(string.Join("\n", report));
+            }
+            else 
+            {
+                await this._next(context);
+            }           
+        }
+    }
+}
---
+++ ApplicationFlowFiltersMiddleware/MiddlewareMoreSpecificThings/CustomizeMiddlewares/Readme.md
@@ -0,0 +1,10 @@
+## Example to user custom middleware
+```
+В реалния живот често ни се случва да имплементираме някаква нова features на
+даден web проект или даден feature e в developer mode. 
+"Real-world apps often implement a concept
+called feature switching, which allows the business or developers to turn parts of 
+the site on or off."
+
+С течение на времето приложението може да има "feature switches" и може да стане трудно бързо да видите кои са включени и изключени, без да гледате конфигурационни файлове.
+```
