de051c13a12a2b4f646e16eee4572d28cc45dc72
mindball <mincho.balaliew@gmail.com>
2021-03-17 14:12:41 +0200

    Seed approach with DI

---
+++ GoodPractice/InitializeSeedingDataApproach/ApproachWithDIMicrosoftVersion/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ApproachWithDIMicrosoftVersion.Data;
+using ApproachWithDIMicrosoftVersion.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ApproachWithDIMicrosoftVersion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RazorPagesMovieContext context;
+
+        public HomeController(RazorPagesMovieContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovie = await this.context.Movies.ToListAsync();
+
+            return View(allMovie);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GoodPractice/InitializeSeedingDataApproach/ApproachWithDIMicrosoftVersion/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using ApproachWithDIMicrosoftVersion.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApproachWithDIMicrosoftVersion.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(
+            DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
