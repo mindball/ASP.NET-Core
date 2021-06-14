335797129ab409d3134b5fed5f4ae067958a86f0
mindball <mincho.balaliew@gmail.com>
2021-06-14 09:05:52 +0300

    Short polling technique

---
+++ SignalR/01. Polling/Controllers/CoffeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary;
+
+namespace _01._Polling.Controllers
+{
+    [Route("[controller]")]
+    public class CoffeeController : Controller
+    {
+        private readonly IOrderService orderService;
+                
+        public CoffeeController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        [HttpPost]
+        public IActionResult OrderCoffee(Order order)
+        {
+            var orderId = this.orderService.NewOrder();
+            return this.Accepted(orderId);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUpdateForOrder(int id)
+        {
+            var result = this.orderService.GetUpdate(id);
+            if (result.New)
+            {
+                return this.Ok(result);
+            }
+
+            return this.NoContent();
+        }
+    }
+}
